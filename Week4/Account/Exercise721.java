b48f1a418e9695f27f7490329b6ba7c2d7a003e3
Injae Lee <94044443+ginidino@users.noreply.github.com>
2021-12-14 16:29:36 +0900

    Delete Week4/Account directory

--- Week4/Account/Exercise721.java
+++
@@ -1,9 +0,0 @@
-package test4;
-
-public class Exercise721 {
-	public static void main(String[] args) {
-		Account bartosAccount = new Account("Barto's account",100.0);
-		bartosAccount.deposit(20.0);
-		System.out.println(bartosAccount);
-	}
-}

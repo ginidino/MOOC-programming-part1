f9d3f8291ac73218ac6dc7aa596d44826cb0c677
Injae Lee <94044443+ginidino@users.noreply.github.com>
2021-12-17 20:03:30 +0900

    Add files via upload

---
+++ Week4/Exercise79/Exercise79.java
@@ -0,0 +1,14 @@
+package test4;
+
+public class Exercise79 {
+	public static void main(String[] args) {
+		NumberStatistics stats = new NumberStatistics();
+		stats.addNumber(3);
+	    stats.addNumber(5);
+	    stats.addNumber(1);
+	    stats.addNumber(2);
+	    System.out.println("Amount: " + stats.amountOfNumbers());
+	    System.out.println("sum: " + stats.sum());
+        System.out.println("average: " + stats.average());
+	}
+}

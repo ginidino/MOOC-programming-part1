dbcc248f880f609a791ce5ed4761bd5dabceb137
Injae Lee <94044443+ginidino@users.noreply.github.com>
2021-12-28 16:23:21 +0900

    Add files via upload

---
+++ Wee1/Exercise6.java
@@ -0,0 +1,12 @@
+package test1;
+
+public class Exercise6 {
+	public static void main(String[] args) {
+		int a = 73457;
+		int b = 12888;
+		int result = a + b;
+		
+		String eqation = a + " + " + b + " = " + result;
+		System.out.println(eqation);
+	}
+}

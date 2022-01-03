5add9b186f92286939c5bbda762b7e4ee524d405
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-01-03 17:12:45 +0900

    Add files via upload

---
+++ Week1/Exercise12.java
@@ -0,0 +1,21 @@
+package test1;
+import java.util.Scanner;
+
+public class Exercise12 {
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Type your name: ");
+		String name1 = reader.nextLine();
+		System.out.print("Type your age: ");
+		int age1 = Integer.parseInt(reader.nextLine());
+		
+		System.out.print("Type your name: ");
+		String name2 = reader.nextLine();
+		System.out.print("Type your age: ");
+		int age2 = Integer.parseInt(reader.nextLine());
+		
+		int sumAge = age1 + age2;
+		
+		System.out.println(name1 + " and " + name2 + " are " + sumAge + " years old in total.");
+	}
+}

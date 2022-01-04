e3adb54e385202b960125803cad348fcf276e0b3
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-01-04 17:06:34 +0900

    Update Exercise2.java

--- Week1/Exercise2.java
+++ Week1/Exercise2.java
@@ -4,7 +4,7 @@ public class Exercise2 {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("(And all the people of the world)");
-		System.out.println();
-		System.out.println("Hello world! \n(And all the people of the world)");
+		/*System.out.println();
+		System.out.println("Hello world! \n(And all the people of the world)");*/
 	}
 }

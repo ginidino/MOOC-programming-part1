ffb207f318f2c35ed59af787038e235f4d4d0cb8
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-01-13 17:18:50 +0900

    Add files via upload

---
+++ Week1/WEEk1/Exercise1.java
@@ -0,0 +1,11 @@
+package test1;
+
+public class Exercise1 {
+	public static void main(String[] args) {
+		System.out.println("Jane Doe");
+		System.out.println();
+		
+		String name = "Jane Doe";
+		System.out.println(name);
+	}
+}
---
+++ Week1/WEEk1/Exercise2.java
@@ -0,0 +1,10 @@
+package test1;
+
+public class Exercise2 {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		System.out.println("(And all the people of the world)");
+		System.out.println();
+		System.out.println("Hello world! \n(And all the people of the world)");
+	}
+}

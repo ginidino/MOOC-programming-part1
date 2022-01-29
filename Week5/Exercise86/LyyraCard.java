3ffb6f152f4e6d03af0b1ca9ae41e99603482509
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-01-29 20:05:21 +0900

    Add files via upload

---
+++ Week5/Exercise86/LyyraCard.java
@@ -0,0 +1,25 @@
+package test5;
+
+public class LyyraCard {
+	private double balance;
+
+	public LyyraCard(double balance) {
+		this.balance = balance;
+	}
+
+	public double balance() {
+		return this.balance;
+	}
+	
+	public void loadMoney(double amount) {
+		this.balance += amount;
+	}
+
+	public boolean pay(double amount) {
+		if (this.balance < amount) {
+			return false;
+		}
+		this.balance -= amount;
+		return true;
+	}
+}

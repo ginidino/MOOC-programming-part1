2c0274418df51879726e131175b3253c92e87231
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-02-16 16:16:04 +0900

    Add files via upload

---
+++ Week6/Exercise97/Exercise97.java
@@ -0,0 +1,17 @@
+package test6;
+
+public class Exercise97 {
+	public static void main(String[] args) {
+        int[] array = {5, 1, 3, 4, 2};
+        printElegantly(array);
+    }
+
+	public static void printElegantly(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i]);
+			if (i < array.length-1) {
+				System.out.print(", ");
+			} 
+		}
+	}
+}

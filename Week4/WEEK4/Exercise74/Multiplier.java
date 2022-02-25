42c7d0613e6920ddf97688fc3b01496f4ccb11d5
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-02-26 00:56:10 +0900

    Add files via upload

---
+++ Week4/WEEK4/Exercise74/Multiplier.java
@@ -0,0 +1,13 @@
+package WEEK4;
+
+public class Multiplier {
+	private int num;
+	
+	public Multiplier(int num) {
+		this.num = num;
+	}
+
+	public int multiply(int otherNumber) {
+		return this.num * otherNumber;
+	}
+}

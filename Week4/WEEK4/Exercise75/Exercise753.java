1086bd431b9485d5a7ddbd46feb26d64146e3826
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-03-02 18:14:27 +0900

    Update Exercise753.java

--- Week4/WEEK4/Exercise75/Exercise753.java
+++ Week4/WEEK4/Exercise75/Exercise753.java
@@ -1,15 +1,16 @@
 package WEEK4;
 
+// Exercise 75.3: Counter reset
 public class Exercise753 {
 	public static void main(String[] args) {
 		DecreasingCounter counter = new DecreasingCounter(100);
 
-        counter.printValue();
+        	counter.printValue();
 
-        counter.reset();
-        counter.printValue();
+        	counter.reset();
+        	counter.printValue();
 
-        counter.decrease();
-        counter.printValue();
+        	counter.decrease();
+        	counter.printValue();
 	}
 }

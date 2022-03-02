912115c78c9c04f9504ac8f5788c1f00d1dcaecd
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-03-02 18:13:06 +0900

    Update Exercise751.java

--- Week4/WEEK4/Exercise75/Exercise751.java
+++ Week4/WEEK4/Exercise75/Exercise751.java
@@ -1,15 +1,16 @@
 package WEEK4;
 
+// Exercise 75.1: Implementing method decrease()
 public class Exercise751 {
 	public static void main(String[] args) {
 		DecreasingCounter counter = new DecreasingCounter(10);
 
-        counter.printValue();
+        	counter.printValue();
 
-        counter.decrease();
-        counter.printValue();
+        	counter.decrease();
+        	counter.printValue();
 
-        counter.decrease();
-        counter.printValue();
+        	counter.decrease();
+        	counter.printValue();
 	}
 }

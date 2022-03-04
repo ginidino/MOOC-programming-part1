5b481cc3feffcf6effae632d93ce1f664391dd11
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-03-04 16:38:32 +0900

    Add files via upload

---
+++ Week4/WEEK4/Exercise81/Test/PasswordRandomizerTest.java
@@ -0,0 +1,20 @@
+package Test;
+
+import java.util.Scanner;
+import java.util.Random;
+
+// create random password by using lowercase
+public class PasswordRandomizerTest {
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		Random random = new Random();
+		System.out.println("Enter a length of password");
+		int len = Integer.parseInt(reader.nextLine());
+		String password = "";
+		for (int i = 0; i < len; i++) {
+			char symbol = "abcdefghijklmnopqrstuvwxyz".charAt(random.nextInt(26));
+			password += symbol;
+		}
+		System.out.println(password);
+	}
+}

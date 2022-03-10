b62d2d19a5d26af8fadee59cb742384560d9e617
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-03-10 17:53:56 +0900

    Add files via upload

---
+++ Week5/WEEK5/Exercise89/BoundedCounter.java
@@ -0,0 +1,45 @@
+package WEEK5;
+
+// Exercise 89: Clock object
+//copy here the class BoundedCounter from last weeks assignment 78
+public class BoundedCounter {
+	private int value;
+    private int upperLimit;
+
+    public BoundedCounter(int upperLimit) {
+        this.value = 0;
+        this.upperLimit = upperLimit;
+    }
+    // 78.1: BoundedCounter
+	public void next() {
+        if (this.value == this.upperLimit) {
+        	this.value = 0;
+        } else {
+        	this.value++;
+        }
+    }
+
+    public String toString() {
+    	// 78.1: BoundedCounter
+        //return "" + this.value;
+    	
+    	// 78.2: Printing the initial zero
+    	String initial = "0";
+    	if (this.value > 9) {
+    		initial = "";
+    	}
+    	return initial + this.value;
+    }
+    // 78.3: The first version of the clock
+    public int getValue() {
+        // write here code that returns the value
+    	return this.value;
+    }
+    // 78.4: The second version of the clock
+	public void setValue(int value) {
+		if (this.value < 0 || this.value > this.upperLimit) {
+			return;
+		}
+		this.value = value;
+	}
+}
---
+++ Week5/WEEK5/Exercise89/Exercise89.java
@@ -0,0 +1,13 @@
+package WEEK5;
+
+// Exercise 89: Clock object
+public class Exercise89 {
+	public static void main(String[] args) {
+		Clock clock = new Clock(23, 59, 50);
+		
+		for (int i = 0; i < 20; i++) {
+			System.out.println( clock );
+            clock.tick();
+		}
+	}
+}

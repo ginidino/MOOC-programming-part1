21a37bd413be17ceb801451d83a133c2d91c8df6
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-03-15 18:05:00 +0900

    Create ch23_9.java

---
+++ Week5/WEEK5/Exercise93/chapter 23.9/ch23_9.java
@@ -0,0 +1,8 @@
+public static void main(String[] args) {
+    Person martin = new Person("Martin", 24, 4, 1983);
+
+    Person juhana = new Person("Juhana", 17, 9, 1985);
+
+    System.out.println(martin);
+    System.out.println(juhana);
+}
